ace48169ac844b85c9ce87f5308fd9f512aaf56f
S.NIRMAL <nirmal.s2022cse@sece.ac.in>
2024-02-27 09:58:31 +0530

    new commit

--- MERN stack/objects.js
+++ MERN stack/objects.js
@@ -1,3 +1,6 @@
 /**
  * 
  */
+const students = ['nirmal','praveen','kesavan','nithish','peer']
+const cgpa = [9.1, 8.6, 9.2, 9.2]
+const locality = ['pollachi','madurai','tirunelveli','namakkal','tirunelveli']
