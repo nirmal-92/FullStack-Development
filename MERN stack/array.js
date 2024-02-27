5ecd32cb1e607a92e0a44c6f7f1fdfcad3637256
S.NIRMAL <nirmal.s2022cse@sece.ac.in>
2024-02-27 09:41:13 +0530

    map and filter commit

--- MERN stack/array.js
+++ MERN stack/array.js
@@ -1,4 +1,31 @@
-const students = ['nithin', 'nirmal', '92', '61'];
-console.log(students);
-students.sort();
-console.log(students);
+/**
+ * push
+ * pop
+ * map - used to add functionality or operations
+ * sort - changes made in the same array
+ * filter - changes made in the array are stored in another array
+ */
+
+const points = ['76', '32', '56', '92', '61'];
+points.sort();
+console.log(points);
+const res = points.filter(function(x){
+    return x > 80
+})
+console.log(res)
+
+const res2 = points.map(function(a){
+    return a*10
+})
+console.log(res2)
+
+let m = 5
+let n = 5
+//double equals == - loose equality
+//triple equals === - strict equality
+if (m == n){
+    console.log('equal')
+}
+else{
+    console.log('not equal')
+}
