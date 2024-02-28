d791515b3731886b5e85e8aa697308c62f8a6b7d
S.NIRMAL <nirmal.s2022cse@sece.ac.in>
2024-02-28 12:34:12 +0530

    data sync

---
+++ MERN stack/async.js
@@ -0,0 +1,13 @@
+function getData() {
+    // creating some delay to match up for real time delay
+    setTimeout(() => {
+        return 'mobiles'
+    }, 2000)
+}
+// Displaying data in the UI
+function displayData(data) {
+    console.log(data)
+}
+
+const data =  getData()
+displayData(data)
