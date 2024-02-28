1598d28f7c38eb6a19d368535a7b171301667c39
S.NIRMAL <nirmal.s2022cse@sece.ac.in>
2024-02-28 15:21:52 +0530

    new

--- MERN stack/promise.js
+++ MERN stack/promise.js
@@ -7,6 +7,26 @@
 
 const newPromise = new Promise(function
 (resolve, reject) {
-    reject()
+    /**
+     * resolve()
+     * promiseState : fulfilled
+     * promiseResult : 'abcd'
+     */
+    /**
+     * resolve('abcd')
+     * promiseState : fulfilled
+     * promiseResult : undefined
+     */
+    /**
+     * reject(123)
+     * promiseState : rejected
+     * promiseResult : 123
+     */
+    reject('efgh')
+    resolve('abcd')
 })
 console.log(newPromise)
+
+newPromise.then(function(){
+    console.log('executing....')
+})
