6a8e7ad06e7edcb984e14a7dd91a952c30c3af19
S.NIRMAL <nirmal.s2022cse@sece.ac.in>
2024-03-01 10:40:42 +0530

    nodemon commit

--- MERN stack/NodeJS/server.js
+++ MERN stack/NodeJS/server.js
@@ -7,14 +7,15 @@
  * to execute : node <filename>
  * npm install <package_name> / npm i <package_name>
  * npm uninstall <package_name>
- * nodemon
+ * npm install nodemon --save-dev
+ * npx nodemon <filename>
  */
 /**
  * npm init
  * npm init -y
  */
 console.log('Hello')
-const arr = [1,2,3,4,5]
+const arr = [1,2,3]
 for(const n of arr){
     console.log(n * 4)
 }
