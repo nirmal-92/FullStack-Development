2c2e2bcc2fa366a09b05fbfbcc821e76f965881d
S.NIRMAL <nirmal.s2022cse@sece.ac.in>
2024-03-01 12:52:09 +0530

    IMDb commit

--- MERN stack/NodeJS/server.cjs
+++ MERN stack/NodeJS/server.cjs
@@ -17,8 +17,8 @@ const fs = require('fs')
  * npm init -y
  */
 /**
- * read/write file - Asynchronous
- * read/write fileSync - Synchronous - stores data
+ * read/write file - Asynchronous - doesnot wait for long time execution
+ * read/write fileSync - Synchronous - stores data - executes only when the readSyncFile is completed
  */
 console.log('Hello')
 //serealize - JSON.stringyfy()
@@ -40,7 +40,15 @@ console.log('Hello')
  * })
  */
 
-
-
-
-
+fs.readFile('./data.json', function(error, file){
+    const data = JSON.parse(file)
+    //console.log(data.IMDb)
+    console.log(data)
+    data.IMDb = 8.7
+    console.log(data)
+    // fs.writeFileSync('./data.json',JSON.stringify(data))
+    fs.writeFile('./data.json', JSON.stringify(data),
+    function(error){
+        console.log('success')
+    })
+})
